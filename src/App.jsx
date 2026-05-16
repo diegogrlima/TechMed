ae762d932f917791388f26946929c14cfb422715
Diego Lima <diegolimareis021@gmail.com>
2026-05-16 11:14:48 -0300

    chore: configurando projeto react vite com boostrap 5

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Button variant="primary">Primary</Button>
+    </>
+  );
+}
+
+export default App;
